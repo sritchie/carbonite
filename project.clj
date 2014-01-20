c16a60bd7d869dda2d4a1d2794f2d2a2cfee75ed
Soren Macbeth <soren@dopeness.org>
2014-01-20 10:18:21 -0800

    warn of reflection

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.esotericsoftware.kryo/kryo "2.21"]
                  [com.twitter/chill-java "0.3.5"]]
+  :global-vars {*warn-on-reflection* true}
   :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
