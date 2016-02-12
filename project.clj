84c7fcdafb5aad6ba33be077879d55f8c3a346fb
Soren Macbeth <soren@dopeness.org>
2016-02-12 12:01:37 -0800

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.twitter/carbonite "1.5.0-SNAPSHOT"
+(defproject com.twitter/carbonite "1.5.0"
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
   :javac-options ["-source" "1.6" "-target" "1.6"]
